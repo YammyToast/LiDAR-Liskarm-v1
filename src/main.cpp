eae187a1f95f8efd3e4a121c1a0629acdb3afac6
YammyToast <cyanjamesmail@gmail.com>
2024-10-27 20:26:08 +0000

    transfer

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "spdlog/spdlog.h"
+
+int main(int argc, char **argv) {
+    printf("Hello World");
+
+    auto console_sink = std::make_shared
+
+    return 0;
+
+} 
